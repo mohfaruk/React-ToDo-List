5cd146cb2e08d1c26f3024502509e04746e747f3
Mohamad Faruk <mohamadfaruk.uk~gmail.com>
2022-01-16 12:59:23 +0000

    Included update feature

--- src/Container.js
+++ src/Container.js
@@ -4,27 +4,39 @@ import Form from "./Form";
 import "./Container.css";
 
 function Container() {
-  const [todos, setTodos] = useState([
-    { task: "Coding", complete: false },
-    { task: "Gym", complete: false },
-    { task: "Laundry", complete: false },
-  ]);
+  const [todos, setTodos] = useState([]);
 
   const createTask = task => {
-    const newTasks = [...todos, { task, complete: false }];
-    setTodos(newTasks);
+    const newTasks = [...todos, { task, complete: false }]; //Leaves existing tasks and Creates a task with string and boolean
+    setTodos(newTasks); //Sets todos state with new addition
+  };
+
+  const updateTask = index => {
+    const newTasks = [...todos]; //Takes existing todo tasks
+    const item = newTasks[index]; //Item selected is placed in variable
+    const updateTask = prompt(`Update ${item.task} task?`, item.task); //Asks for update, and new value is placed in variable
+    const newItem = { task: updateTask, complete: false }; //Creates new object with updated value
+    newTasks.splice(index, 1, newItem); //Deletes selected item and replaces with updated item
+
+    //If user enters no value then return immediately
+    //Otherwise todos stats changes to updated tasks
+    if (updateTask === "" || updateTask === null) {
+      return;
+    } else {
+      setTodos(newTasks);
+    }
   };
 
   const removeTask = index => {
-    const newTasks = [...todos];
-    newTasks.splice(index, 1);
-    setTodos(newTasks);
+    const newTasks = [...todos]; //Takes existing todo tasks
+    newTasks.splice(index, 1); //Deletes only onet task from index number
+    setTodos(newTasks); //Sets todo state to existing tasks after deletion
   };
 
   const toggleTask = index => {
-    const newTasks = [...todos];
-    newTasks[index].complete = !newTasks[index].complete;
-    setTodos(newTasks);
+    const newTasks = [...todos]; //Takes existing todo tasks
+    newTasks[index].complete = !newTasks[index].complete; //Changes to opposite of whatever the boolean value is
+    setTodos(newTasks); //Updates todos state
   };
 
   return (
@@ -37,6 +49,7 @@ function Container() {
             <Todo
               todo={todo}
               index={index}
+              update={updateTask}
               remove={removeTask}
               check={toggleTask}
             />
--- src/Todo.js
+++ src/Todo.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./Todo.css";
 
-const Todo = ({ todo, index, remove, check }) => {
+const Todo = ({ todo, index, update, remove, check }) => {
+  //Passes down props
   return (
     <div className="Todo">
       <li
-        className={todo.complete ? "Todo-completed" : ""}
+        className={todo.complete ? "Todo-completed" : ""} //If task is complete, add completed class
         onClick={() => {
           check(index);
         }}
@@ -15,10 +16,17 @@ const Todo = ({ todo, index, remove, check }) => {
       <div className="Todo-buttons">
         <button
           onClick={() => {
+            update(index);
+          }}
+        >
+          <i class="far fa-edit"></i>
+        </button>
+        <button
+          onClick={() => {
             remove(index);
           }}
         >
-          <i class="fas fa-trash" />
+          <i className="fas fa-trash-alt" />
         </button>
       </div>
     </div>
